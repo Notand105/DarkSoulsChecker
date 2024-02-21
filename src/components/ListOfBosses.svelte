a3af918cc50ce69b3359273a782d42e3f1f77aba
Notand105 <urzua.fernando.f@gmail.com>
2024-02-21 19:37:21 -0300

    upgrades

--- src/components/ListOfBosses.svelte
+++ src/components/ListOfBosses.svelte
@@ -9,29 +9,33 @@
     }
 </script>
 
-<section class="w-5/6 m-auto">
-    <div
-        class="flex flex-col  gap-4 container "
-    >
+<section class="w-5/6 m-auto border container py-4">
+    <div class="flex flex-col md:flex-row  flex-wrap gap-4 md:gap-8 justify-center">
         {#each data as boss}
             <div
-                class="flex justify-between sm:h-32 md:h-40 rounded-md border-red-200 border sm:flex-row flex-col  gap-4 "
+                class="flex justify-between  rounded-md border-gray-500 shadow-xl border flex-col gap-4 lg:w-1/3 "
                 class:gray={boss.defeated}
+                class:small={boss.defeated}
+                class:big={!boss.defeated}
             >
                 <img
-                    class="aspect-auto rounded-t-md sm:rounded-s-md"
+                    class="aspect-auto rounded-t-md "
                     src={boss.image}
                     alt={boss.name}
                 />
-                <h3>{boss.name}</h3>
-                <div class="flex flex-col gap-2 p-2">
-                    <span>
+                    
+                <div class="flex flex-col items-center">
+                    <h3>{boss.name}</h3>
+                    <h4>Location: {boss.location}</h4>
+                    <h4>Health: {boss.health}</h4>
+                    <h4>Souls: {boss.souls}</h4>
+                </div>
+
+                <div class="text-center pr-2 pb-4 ">
+                    <button class="bg-red-800 text-white py-2 px-4 rounded-md" on:click={()=> handleClick(boss.id)}>
                         Defeated
-                        <input
-                            type="checkbox"
-                            on:input={() => handleClick(boss.id)}
-                        />
-                    </span>
+                    </button>
+                    
                 </div>
             </div>
         {/each}
@@ -42,5 +46,13 @@
     .gray {
         -webkit-filter: grayscale(1);
         filter: grayscale(1);
+    }
+    .small {
+        transform: scale(.9);
+        transition: .3s ease;
+    }
+    .big{
+        transform: scale(1);
+        transition: .3s ease;
     }
 </style>
