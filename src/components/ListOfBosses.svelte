9d85ef0c497b3bf6b6a98492145b6381e571249e
Notand105 <urzua.fernando.f@gmail.com>
2024-02-26 11:26:55 -0300

    colors and more images

--- src/components/ListOfBosses.svelte
+++ src/components/ListOfBosses.svelte
@@ -1,19 +1,21 @@
 <script>
-    import bosses from "../data/bosses.json";
-    let data = bosses;
+    //import bosses from "../data/bosses.json";
+    import {List} from '../stores/bosses_store'
+    let data = $List;
 
     function handleClick(id) {
         console.log(data);
         let negated = !data[id].defeated;
         data[id].defeated = negated;
+        localStorage.setItem('listOfBosses', JSON.stringify(data))
     }
 </script>
 
-<section class="w-5/6 m-auto border container py-4">
+<section class="w-5/6 m-auto container py-4">
     <div class="flex flex-col md:flex-row  flex-wrap gap-4 md:gap-8 justify-center">
         {#each data as boss}
             <div
-                class="flex justify-between  rounded-md border-gray-500 shadow-xl border flex-col gap-4 lg:w-1/3 "
+                class="flex justify-between  rounded-md  shadow-md flex-col gap-4 lg:w-1/3 softDark"
                 class:gray={boss.defeated}
                 class:small={boss.defeated}
                 class:big={!boss.defeated}
@@ -25,14 +27,14 @@
                 />
                     
                 <div class="flex flex-col items-center">
-                    <h3>{boss.name}</h3>
-                    <h4>Location: {boss.location}</h4>
+                    <h3 class="text-xl font-bold">{boss.name}</h3>
+                    <h4 class="text-lg primary italic">Location: {boss.location}</h4>
                     <h4>Health: {boss.health}</h4>
                     <h4>Souls: {boss.souls}</h4>
                 </div>
 
-                <div class="text-center pr-2 pb-4 ">
-                    <button class="bg-red-800 text-white py-2 px-4 rounded-md" on:click={()=> handleClick(boss.id)}>
+                <div class="text-center p-4 pb-6 ">
+                    <button class="accent  py-2 rounded-md w-full" on:click={()=> handleClick(boss.id)}>
                         Defeated
                     </button>
                     
@@ -46,6 +48,7 @@
     .gray {
         -webkit-filter: grayscale(1);
         filter: grayscale(1);
+        opacity: .8;
     }
     .small {
         transform: scale(.9);
@@ -55,4 +58,18 @@
         transform: scale(1);
         transition: .3s ease;
     }
+    .softDark{
+        background-color: #0F1920;
+    }
+    .accent{
+        background-color: #bd6946;
+    }
+    .primary{
+        /* background-color: #F4EFDE; */
+        color: #9ec8dc;
+    }
+    /* .secondary{
+        color: #7e2d4c;
+    } */
+    
 </style>
---
+++ src/stores/bosses_store.js
@@ -0,0 +1,7 @@
+import { writable } from "svelte/store";
+import uncheckedBosses from '../data/bosses.json'
+
+let localStorageBossesList = window.localStorage.getItem('listOfBosses')
+localStorageBossesList = JSON.parse(localStorageBossesList)
+
+export const List = writable(localStorageBossesList || uncheckedBosses)
