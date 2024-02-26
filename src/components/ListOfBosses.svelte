7aa7360951e1b9713c55c83f34ada6e1deff1cf9
Notand105 <urzua.fernando.f@gmail.com>
2024-02-26 19:00:00 -0300

    hero

--- src/components/ListOfBosses.svelte
+++ src/components/ListOfBosses.svelte
@@ -1,46 +1,71 @@
 <script>
-    //import bosses from "../data/bosses.json";
-    import {List} from '../stores/bosses_store'
+    import { List } from "../stores/bosses_store";
     let data = $List;
 
     function handleClick(id) {
-        console.log(data);
         let negated = !data[id].defeated;
         data[id].defeated = negated;
-        localStorage.setItem('listOfBosses', JSON.stringify(data))
+        localStorage.setItem("listOfBosses", JSON.stringify(data));
+    }
+
+    function resetBosses() {
+        localStorage.removeItem("listOfBosses");
+        let newData = [];
+        for (let i = 0; i < data.length; i++) {
+            let aux = data[i];
+            aux.defeated = false;
+            newData.push(aux);
+        }
+        data = [...newData];
     }
 </script>
 
-<section class="w-5/6 m-auto container py-4">
-    <div class="flex flex-col md:flex-row  flex-wrap gap-4 md:gap-8 justify-center">
+<section class="w-5/6 m-auto container py-4 mt-10">
+        <div
+        class="flex flex-col md:flex-row flex-wrap gap-4 md:gap-8 justify-center"
+    >
         {#each data as boss}
             <div
-                class="flex justify-between  rounded-md  shadow-md flex-col gap-4 lg:w-1/3 softDark"
+                class="flex justify-between rounded-md shadow-md flex-col gap-4 lg:w-1/3 softDark"
                 class:gray={boss.defeated}
                 class:small={boss.defeated}
                 class:big={!boss.defeated}
             >
                 <img
-                    class="aspect-auto rounded-t-md "
+                    class="aspect-auto rounded-t-md"
                     src={boss.image}
                     alt={boss.name}
+                    loading="lazy"
                 />
-                    
+
                 <div class="flex flex-col items-center">
-                    <h3 class="text-xl font-bold text-center"><span class="text-sm secondary">#{boss.id + 1 } </span>{boss.name}</h3>
-                    <h4 class="text-lg primary italic">Location: {boss.location}</h4>
+                    <h3 class="text-xl font-bold text-center">
+                        <span class="text-sm secondary"
+                            >#{boss.id + 1}
+                        </span>{boss.name}
+                    </h3>
+                    <h4 class="text-lg primary italic">
+                        Location: {boss.location}
+                    </h4>
                     <h4>Health: {boss.health}</h4>
                     <h4>Souls: {boss.souls}</h4>
                 </div>
 
-                <div class="text-center p-4 pb-6 ">
-                    <button class="accent  py-2 rounded-md w-full" on:click={()=> handleClick(boss.id)}>
+                <div class="text-center p-4 pb-6">
+                    <button
+                        class="accent py-2 rounded-md w-full"
+                        on:click={() => handleClick(boss.id)}
+                    >
                         Defeated
                     </button>
-                    
                 </div>
             </div>
         {/each}
+    </div>
+    <div class="flex justify-center my-4">
+        <button on:click={resetBosses} class="secondaryBg p-4 rounded w-1/2"
+            >Reset bosses</button
+        >
     </div>
 </section>
 
@@ -48,28 +73,38 @@
     .gray {
         -webkit-filter: grayscale(1);
         filter: grayscale(1);
-        opacity: .8;
+        opacity: 0.8;
     }
     .small {
-        transform: scale(.9);
-        transition: .3s ease;
+        transform: scale(0.9);
+        transition: 0.3s ease;
     }
-    .big{
+    .big {
         transform: scale(1);
-        transition: .3s ease;
+        transition: 0.3s ease;
     }
-    .softDark{
-        background-color: #0F1920;
+    .softDark {
+        background-color:#170E0F ;
+
     }
-    .accent{
-        background-color: #bd6946;
+    .accent {
+        background-color: #8a2730;
     }
-    .primary{
+    /* .accentText{
+        color: #bd6946;
+    } */
+    .primary {
         /* background-color: #F4EFDE; */
         color: #9ec8dc;
     }
-     .secondary{
-        color: #7e2d4c;
+    /* .primaryBg {
+        background-color: #9ec8dc;
+    } */
+    .secondary {
+        color: #dd3a47;
     }
-    
+    .secondaryBg {
+        background-color: #dd3a47;
+    }
+
 </style>
